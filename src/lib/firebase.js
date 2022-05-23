f96b98dbb62f161c1e85fb802850750d8420f212
Bram Sikkens <bram_sikkens@hotmail.com>
2022-05-23 15:56:51 +0200

    firebase env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAAeD06X_Vyq1bIHX9DrgKsFaPo-Vfyf4o",
-  authDomain: "netflixclone-e1f7e.firebaseapp.com",
-  projectId: "netflixclone-e1f7e",
-  storageBucket: "netflixclone-e1f7e.appspot.com",
-  messagingSenderId: "417578012994",
-  appId: "1:417578012994:web:b29846dac71e97084578db",
-  measurementId: "G-ZLRG2LY1F3",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
